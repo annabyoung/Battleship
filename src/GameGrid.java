28f152748f1caa08e3ee9aed3e217cb1faf6019d
Annabelle Young <annabyoung@gmail.com>
2016-07-20 20:43:36 +0100

    Ship icon added to button and fixed (x,y)

--- src/GameGrid.java
+++ src/GameGrid.java
@@ -5,35 +5,22 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 public class GameGrid {
-	JFrame gameFrame = new JFrame("Game Grid"); //create gameFrame
+	private JFrame gameFrame = new JFrame("Game Grid"); //create gameFrame
 	private JButton[][] grid; //names the grid of buttons
-	private JPanel gamePanel = new JPanel();
-	
-	public GameGrid() {
-		try {
-			UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		
-		this.gameFrame = gameFrame;
-		//panel's settings
-		gamePanel.setLayout(new GridBagLayout());
-		gamePanel.setPreferredSize(new Dimension(340,340));
-		gamePanel.setBackground(new Color(131, 209, 232));
-		gamePanel.setBorder(BorderFactory.createLineBorder(new Color(32,156,185)));
-		
-		//this.setTitle("Battleship");
-	}
 	
 	public GameGrid(int width, int length){ //take grid parameters
 		//set grid's layout
@@ -53,52 +40,26 @@ public class GameGrid {
 					grid[x][y] = new JButton(letters[y]);
 					gameFrame.add(grid[x][y]); 
 				}
+				else if (x == 2 && y == 2){
+					grid[x][y] = new JButton();
+					try { //insert image onto a certain tile
+						Image img = ImageIO.read(getClass().getResource("/images/battleship-1.png"));
+						grid[x][y].setIcon(new ImageIcon(img));
+						gameFrame.add(grid[x][y]);
+						//need to see if image can be stretched to cover two tiles?
+					} catch (IOException ex){}
+				}
 				else {
-					grid[x][y] = new JButton("("+x+","+y+")"); 
+					grid[x][y] = new JButton("("+letters[y]+","+x+")"); 
 					gameFrame.add(grid[x][y]); //adding the buttons to the gameFrame
 				}
 			}
 		}
+
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		gameFrame.pack();
 		gameFrame.setVisible(true);
-	}
-	
-	public void displayShip(String type, int x, int y, boolean horizontal, int length){
-		 //create new button
-		JButton button = new JButton();
-		button.setOpaque(false); 
-		button.setContentAreaFilled(false); //make transparent
-		button.setCursor(new Cursor(Cursor.HAND_CURSOR)); //sets cursor image
-		button.setBorder(BorderFactory.createLineBorder(new Color(32, 156, 185)));
 		
-		//button dimensions
-		int width = 34;
-		int height = 34;
-		
-		//set constraints for new button
-		GridBagConstraints constraint = new GridBagConstraints();
-		constraint.fill = GridBagConstraints.BOTH; //fill display area entirely
-		constraint.gridx = x; 
-		constraint.gridy = y;
-		
-		if(horizontal){
-			//horizontal = true
-			constraint.gridwidth = length;
-			constraint.gridheight = 1;
-			width = 34*length;
-		} else {
-			//horizontal = false AKA vertical
-			constraint.gridwidth  = 1;
-			constraint.gridheight = length;
-			height = 34 * length;
-		}
-		
-		button.setPreferredSize(new Dimension(width, height));
-		
-		gamePanel.add(button, constraint); //add button with constraints to panel
-		gamePanel.revalidate(); //
-		gamePanel.repaint();
 		
 	}
 	
