ba5acd35916fb715f8b498931e04d9572afe6c9a
Annabelle Young <annabyoung@gmail.com>
2016-07-28 17:24:07 +0100

    Display ship on click

--- src/GameGrid.java
+++ src/GameGrid.java
@@ -1,12 +1,14 @@
 
 
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -17,22 +19,25 @@ import javax.swing.JPanel;
 public class GameGrid {
 	private JFrame gameFrame = new JFrame("Game Grid"); //create gameFrame
 	private JPanel gamePanel = new JPanel();//create panel
-	private JButton[][] grid; //names the grid of buttons
-	private Container container = gameFrame.getContentPane();
 	private JButton panelButton;
+	//private static final int N = 11;
+	private ArrayList<JButton> buttons = new ArrayList<JButton>();
 	
-	public GameGrid() {
-		
+	public GameGrid(){}
+	
+	public GameGrid(JFrame gameFrame, JPanel gamePanel) {
+		this.gameFrame = gameFrame;
+		this.gamePanel = gamePanel;
 	}
 	
-	public GameGrid(int width, int length){ //take grid parameters
+	public GameGrid(int width, int length, int bounds1, int bounds2, String title){ //take grid parameters
 		//set grid's layout
 		String letters[] = {" X ","A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 		String numbers[] = {" X ", "1","2","3","4","5","6","7","8","9","10"};
 		gameFrame.setLayout(new GridLayout(width,length));
 		gamePanel.setLayout(new GridBagLayout());
-		gamePanel.setPreferredSize(new Dimension(340, 340));
-		grid = new JButton[width][length];
+		gamePanel.setPreferredSize(new Dimension(800, 800));
+		
 		//nested for loop to display values on each button
 		for (int y=0; y<length; y++){
 			for(int x = 0; x< width; x++){	
@@ -58,22 +63,6 @@ public class GameGrid {
 					constraint.gridy = y;
 					gamePanel.add(panelButton, constraint);
 				}
-				//testing to display ship to hard coded spot
-				else if (x == 2 && y == 2){
-					//grid[x][y] = new JButton();
-					panelButton = new JButton();
-					try { //insert image onto a certain tile
-						Image img = ImageIO.read(getClass().getResource("/images/battleship-4.png"));
-						panelButton.setIcon(new ImageIcon (img));
-						constraint.fill = GridBagConstraints.BOTH;
-						constraint.weightx = .5;
-						constraint.weighty = .5;
-						constraint.gridx = x;
-						constraint.gridy = y;
-						gamePanel.add(panelButton, constraint);
-						//need to see if image can be stretched to cover two buttons?
-					} catch (IOException ex){}
-				}
 				else {
 					panelButton = new JButton("("+letters[y]+","+x+")");
 					constraint.fill = GridBagConstraints.BOTH;
@@ -82,22 +71,74 @@ public class GameGrid {
 					constraint.gridx = x;
 					constraint.gridy = y;
 					gamePanel.add(panelButton, constraint);
+					buttons.add(panelButton);
+					panelButton.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							Image img = null;
+							GridBagConstraints constraint = new GridBagConstraints();
+						
+							JButton gb = (JButton) e.getSource(); //get location of click
+							
+							try {
+								img = ImageIO.read(getClass().getResource("/images/battleship-6.png"));
+					
+								constraint.fill = GridBagConstraints.BOTH;
+								constraint.weightx = .5;
+								constraint.weighty = .5;
+								
+							} catch (IOException exception) {
+								exception.printStackTrace();
+							}
+							gb.setText("");
+							gb.setIcon(new ImageIcon(img)); //set image to icon
+							
+							gamePanel.revalidate();
+							gamePanel.repaint();
+							
+						}
+					});
 				}
 			}
 		}
 		
 		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		//gameFrame.add(gamePanel);
-		//gameFrame.setPreferredSize(new Dimension(400,400));
+		gameFrame.setTitle(title);
 		gameFrame.setContentPane(gamePanel);
+		gameFrame.setBounds(bounds1,bounds2,800,800); //have boards display side by side
 		gameFrame.pack();
 		gameFrame.setVisible(true);
-		
+		GameField gameField = new GameField(gameFrame, gamePanel);
 		
 	}
-	
-	
-	
 
+	public JFrame getGameFrame() {
+		return gameFrame;
+	}
+
+	public void setGameFrame(JFrame gameFrame) {
+		this.gameFrame = gameFrame;
+	}
+
+	public JPanel getGamePanel() {
+		return gamePanel;
+	}
+
+	public void setGamePanel(JPanel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
+	
+	/*
+	public JButton getButton(int w, int z){
+		int index = w * N + z;
+		return buttons.get(index);
+	}
+	
+	public void removeButton(int x, int y){
+		gamePanel.remove(getButton(x,y));
+	}
+	*/
+	
 }
